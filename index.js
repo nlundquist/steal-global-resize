3cb086ff28f13d338238d8fc3a78da1ac7106862
Nils Lundquist <nils@bitovi.com>
2016-02-25 10:41:33 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+/**
+ * Created by nlundqu on 2016-02-25.
+ */
+import resizeSensor from './resize-sensor';
+
+document.body.innerHTML = 'is import function: ' + (typeof resizeSensor === 'function');
